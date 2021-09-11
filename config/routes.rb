ce716290f5442e04518ecc97cdd96c38724ec76a
samanehabbasi <samaneh.8888@gmail.com>
2021-09-11 14:41:25 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
 
   resources :lists do
- resources :bookmarks, only: %i(new create)
+  resources :bookmarks, only: %i(new create)
   end
   resources :bookmarks, only:[:destroy]
 end
